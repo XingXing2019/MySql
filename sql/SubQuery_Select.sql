2234cfc25ab67452e96d8f8b993068caf794b4cb
Xing <x.xing.unsw@gmail.com>
2020-07-18 20:56:32 +1000

    From and exists

--- sql/SubQuery_Select.sql
+++ sql/SubQuery_Select.sql
@@ -18,3 +18,46 @@ SELECT (
 	ON e.department_id = d.department_id
 	WHERE e.employee_id = 102
 ) AS DepartmentName;
+
+
+SELECT avg_dep.AvgSalary, avg_dep.department_id, g.grade_level
+FROM (
+		SELECT AVG(salary) AvgSalary, e.department_id
+		FROM employees e
+		GROUP BY e.department_id
+	) AS avg_dep JOIN job_grades g
+	ON avg_dep.AvgSalary BETWEEN g.lowest_sal AND g.highest_sal;
+
+
+SELECT d.department_name
+FROM departments d
+WHERE EXISTS(
+	SELECT * 
+	FROM employees e 
+	WHERE d.department_id = e.department_id
+);
+
+
+SELECT d.department_name
+FROM departments d
+WHERE d.department_id IN (
+	SELECT e.department_id
+	FROM employees e
+);
+
+
+SELECT b.*
+FROM boys b
+WHERE b.id NOT IN (
+	SELECT boyfriend_id
+	FROM beauty 
+);
+
+
+SELECT b.*
+FROM boys b
+WHERE NOT EXISTS (
+	SELECT boyfriend_id
+	FROM beauty 
+	WHERE b.id = boyfriend_id
+);
